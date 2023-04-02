764ebc09a34e3f8a6e445eb828ddf40a64f31c3f
padams12 <82292085+padams12@users.noreply.github.com>
2023-04-02 10:14:11 -0700

    Add playRound functionality

--- main.js
+++ main.js
@@ -7,3 +7,75 @@ function getComputerChoice() {
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = computerSelection.toLowerCase();
+        
+        if (playerSelection == "rock") {
+
+            if (computerSelection == "paper") {
+
+                let winner = `Computer wins! Player chose ${playerSelection} and computer beat it with ${computerSelection}.`;
+                return winner;
+            }
+
+            else if (computerSelection == playerSelection) {
+
+                let winner = `Tie game. Both computer and player chose ${playerSelection}.`;
+                return winner;
+            }
+
+            else {
+
+                let winner = `Player wins! Player chose ${playerSelection} and computer lost by choosing ${computerSelection}.`;
+                return winner;
+            }
+
+        }
+
+        if (playerSelection == "paper") {
+
+            if (computerSelection == "scissors") {
+
+                let winner = `Computer wins! Player chose ${playerSelection} and computer beat it with ${computerSelection}.`;
+                return winner;
+            }
+
+            else if (computerSelection == playerSelection) {
+
+                let winner = `Tie game. Both computer and player chose ${playerSelection}.`;
+                return winner;
+            }
+
+            else {
+
+                let winner = `Player wins! Player chose ${playerSelection} and computer lost by choosing ${computerSelection}.`;
+                return winner;
+            }
+
+        }
+
+        if (playerSelection == "scissors") {
+
+            if (computerSelection == "rock") {
+
+                let winner = `Computer wins! Player chose ${playerSelection} and computer beat it with ${computerSelection}.`;
+                return winner;
+            }
+
+            else if (computerSelection == playerSelection) {
+
+                let winner = `Tie game. Both computer and player chose ${playerSelection}.`;
+                return winner;
+            }
+
+            else {
+
+                let winner = `Player wins! Player chose ${playerSelection} and computer lost by choosing ${computerSelection}.`;
+                return winner;
+            }
+
+        }
+}
+
